6e4953155d8b0174d99fabd8cb23e76dcea6619d
nikhilreddys <nikhilreddy4560@gmail.com>
2024-02-07 10:51:49 +0530

    First Commit Of Code

---
+++ src/Test1.java
@@ -0,0 +1,17 @@
+
+public class Test1 
+{
+	public static void main(String[] args) {
+		String text = "Yes";
+		
+		if(text == "Yes")
+		{
+			System.out.println("Correct");
+		}
+		else
+		{
+			System.out.println("Incorrect");
+		}
+	}
+
+}
